8a9f5b99b4a30dccf4f036805e36f1926e2f0041
James Carman <james@carmanconsulting.com>
2008-09-09 04:08:23 +0000

    Refactoring a bit.

---
+++ wicketopia/src/main/java/org/wicketopia/service/def/DefaultEditorTypeMapping.java
@@ -0,0 +1,106 @@
+package org.wicketopia.service.def;
+
+import org.apache.commons.lang.ClassUtils;
+import org.wicketopia.service.EditorTypeMapping;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * @since 1.0
+ */
+public class DefaultEditorTypeMapping implements EditorTypeMapping
+{
+//**********************************************************************************************************************
+// Fields
+//**********************************************************************************************************************
+
+    private Map<String, String> editorTypeMap = new HashMap<String, String>();
+
+//**********************************************************************************************************************
+// Constructors
+//**********************************************************************************************************************
+
+    public DefaultEditorTypeMapping()
+    {
+        addEditorTypeOverride(Byte.class, "byte");
+        addEditorTypeOverride(Short.class, "short");
+        addEditorTypeOverride(Integer.class, "integer");
+        addEditorTypeOverride(Long.class, "long");
+        addEditorTypeOverride(Boolean.class, "boolean");
+        addEditorTypeOverride(Character.class, "character");
+        addEditorTypeOverride(Float.class, "float");
+        addEditorTypeOverride(Double.class, "double");
+
+        addEditorTypeOverride(Byte.TYPE, "byte");
+        addEditorTypeOverride(Short.TYPE, "short");
+        addEditorTypeOverride(Integer.TYPE, "integer");
+        addEditorTypeOverride(Long.TYPE, "long");
+        addEditorTypeOverride(Boolean.TYPE, "boolean");
+        addEditorTypeOverride(Character.TYPE, "character");
+        addEditorTypeOverride(Float.TYPE, "float");
+        addEditorTypeOverride(Double.TYPE, "double");
+
+        addEditorTypeOverride(String.class, "string");
+
+        addEditorTypeOverride(Date.class, "date");
+        addEditorTypeOverride(java.sql.Date.class, "sql_date");
+        addEditorTypeOverride(Timestamp.class, "sql_timestamp");
+
+    }
+
+//**********************************************************************************************************************
+// EditorTypeMapping Implementation
+//**********************************************************************************************************************
+
+    public String getEditorType( Class propertyType )
+    {
+        final Queue<Class> typeQueue = createTypeQueue(propertyType);
+        while( !typeQueue.isEmpty() )
+        {
+            Class type = typeQueue.remove();
+            final String editorType = editorTypeMap.get(type.getName());
+            if( editorType != null )
+            {
+                return editorType;
+            }
+        }
+        return null;
+    }
+
+//**********************************************************************************************************************
+// Other Methods
+//**********************************************************************************************************************
+
+    @SuppressWarnings( "unchecked" )
+    private Queue<Class> createTypeQueue( final Class originalType )
+    {
+        Queue<Class> queue = new LinkedList<Class>();
+        Class currentType = originalType;
+        do
+        {
+            queue.add(currentType);
+            currentType = currentType.getSuperclass();
+        }
+        while( currentType != null );
+        queue.addAll(ClassUtils.getAllInterfaces(originalType));
+        return queue;
+    }
+
+    public void addEditorTypeOverride( Class propertyType, String editorType )
+    {
+        editorTypeMap.put(propertyType.getName(), editorType);
+    }
+
+    public void setEditorTypeOverrides( Map<Class, String> editorTypeOverrides )
+    {
+        for( Class propertyType : editorTypeOverrides.keySet() )
+        {
+            editorTypeMap.put(propertyType.getName(), editorTypeOverrides.get(propertyType));
+        }
+    }
+}
---
+++ wicketopia/src/test/java/org/wicketopia/service/def/TestDefaultEditorTypeMapping.java
@@ -0,0 +1,94 @@
+package org.wicketopia.service.def;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+import org.testng.annotations.Test;
+import org.wicketopia.util.Person;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @since 1.0
+ */
+public class TestDefaultEditorTypeMapping
+{
+    @Test
+    public void testDefaultMappings()
+    {
+        DefaultEditorTypeMapping impl = new DefaultEditorTypeMapping();
+        assertEquals("byte", impl.getEditorType(Byte.class));
+        assertEquals("byte", impl.getEditorType(Byte.TYPE));
+        assertEquals("short", impl.getEditorType(Short.class));
+        assertEquals("short", impl.getEditorType(Short.TYPE));
+        assertEquals("integer", impl.getEditorType(Integer.class));
+        assertEquals("integer", impl.getEditorType(Integer.TYPE));
+        assertEquals("long", impl.getEditorType(Long.class));
+        assertEquals("long", impl.getEditorType(Long.TYPE));
+
+        assertEquals("float", impl.getEditorType(Float.class));
+        assertEquals("float", impl.getEditorType(Float.TYPE));
+        assertEquals("double", impl.getEditorType(Double.class));
+        assertEquals("double", impl.getEditorType(Double.TYPE));
+
+        assertEquals("character", impl.getEditorType(Character.class));
+        assertEquals("character", impl.getEditorType(Character.TYPE));
+
+        assertEquals("string", impl.getEditorType(String.class));
+
+        assertEquals("date", impl.getEditorType(Date.class));
+    }
+
+    @Test
+    public void testAddOverride()
+    {
+        DefaultEditorTypeMapping impl = new DefaultEditorTypeMapping();
+        impl.addEditorTypeOverride(Integer.class, "integer_override");
+        assertEquals("integer_override", impl.getEditorType(Integer.class));
+    }
+
+    @Test
+    public void testSetOverrides()
+    {
+        DefaultEditorTypeMapping impl = new DefaultEditorTypeMapping();
+        final Map<Class, String> overrides = new HashMap<Class, String>();
+        overrides.put(Integer.class, "integer_override");
+        impl.setEditorTypeOverrides(overrides);
+        assertEquals("integer_override", impl.getEditorType(Integer.class));
+    }
+
+    @Test
+    public void testSuperclassMatch()
+    {
+        DefaultEditorTypeMapping impl = new DefaultEditorTypeMapping();
+        impl.addEditorTypeOverride(RuntimeException.class, "exception");
+        assertEquals("exception", impl.getEditorType(IllegalArgumentException.class));
+    }
+
+    @Test
+    public void testInterfaceMatch()
+    {
+        DefaultEditorTypeMapping impl = new DefaultEditorTypeMapping();
+        impl.addEditorTypeOverride(Serializable.class, "serial");
+        assertEquals("serial", impl.getEditorType(Person.class));
+    }
+
+    @Test
+    public void testNoMatch()
+    {
+        DefaultEditorTypeMapping impl = new DefaultEditorTypeMapping();
+        assertNull(impl.getEditorType(Connection.class));
+    }
+    /*
+    @Test
+    public void testSettingMap()
+    {
+        DefaultEditorTypeMapping impl = new DefaultEditorTypeMapping();
+        final Map<Class,String> map = new HashMap<Class,String>();
+        impl.setEditorTypeMap(map);
+        assertNull(impl.getEditorType(String.class));
+    }*/
+}
